85cc8c20830c120826342b7182217fa6a70edf5e
Jordi Esteve <jesteve@zikzakmedia.com>
2014-06-04 19:54:57 +0200

    Fix creation of stock moves with unit_prices using more than 4 decimals

---
+++ move.py
@@ -0,0 +1,20 @@
+# This file is part of sale_discount module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.pool import PoolMeta
+from trytond.config import CONFIG
+DIGITS = int(CONFIG.get('unit_price_digits', 4))
+DISCOUNT_DIGITS = int(CONFIG.get('discount_digits', 4))
+
+__all__ = ['Move']
+__metaclass__ = PoolMeta
+
+
+class Move:
+    __name__ = 'stock.move'
+
+    @classmethod
+    def __setup__(cls):
+        super(Move, cls).__setup__()
+        cls.unit_price.digits = (20, DIGITS + DISCOUNT_DIGITS)
