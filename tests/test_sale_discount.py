ff997bb22525ec4148f22ffcbae596a87e8c570a
resteve <resteve@zikzakmedia.com>
2015-12-01 11:18:48 +0100

    Tests

--- tests/test_sale_discount.py
+++ tests/test_sale_discount.py
@@ -1,33 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the sale_discount module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleDiscountTestCase(unittest.TestCase):
+class SaleDiscountTestCase(ModuleTestCase):
     'Test Sale Discount module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_discount')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_discount')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_discount'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            SaleDiscountTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_sale.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+        SaleDiscountTestCase))
     return suite
