7c6fad008175a1e2ee88d532d635addbdab3ef50
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-05 19:20:42 +0100

    Use price digits (grafted from ed0e50ef31395dbcdaa92d0c7036c533de542f10)

--- move.py
+++ move.py
@@ -3,12 +3,12 @@
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 from trytond.config import config as config_
+from trytond.modules.product import price_digits
 
 
 __all__ = ['Move']
 __metaclass__ = PoolMeta
 
-DIGITS = config_.getint('product', 'price_decimal', default=4)
 DISCOUNT_DIGITS = config_.getint('product', 'discount_decimal', default=4)
 
 
@@ -18,7 +18,7 @@ class Move:
     @classmethod
     def __setup__(cls):
         super(Move, cls).__setup__()
-        cls.unit_price.digits = (20, DIGITS + DISCOUNT_DIGITS)
+        cls.unit_price.digits = (20, price_digits[1] + DISCOUNT_DIGITS)
         #Compatibility with purchase_shipment_cost
         if hasattr(cls, 'unit_shipment_cost'):
             cls.unit_shipment_cost.digits = cls.unit_price.digits
