a9fb638f67becf298f0989026c93d8ca0433c6fa
Raimon Esteve <raimon@nan-tic.com>
2016-11-25 16:29:40 +0100

    Remove duplicated line in on_change_product

--- sale.py
+++ sale.py
@@ -161,7 +161,6 @@ class SaleLine:
         self.discount = Decimal(0)
 
         if self.unit_price:
-            self.gross_unit_price = self.unit_price
             self.update_prices()
 
     @fields.depends('discount', 'unit_price', '_parent_sale.sale_discount')
